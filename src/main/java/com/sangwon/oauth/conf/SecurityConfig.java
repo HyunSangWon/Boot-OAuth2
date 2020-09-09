231a2febfa7afdf429c98d34abb51cfe1a23d8d8
HyunSangWon <hyunsangwon93@gmail.com>
2020-09-09 11:36:06 +0900

    OAuth2 구글 로그인 추가

--- src/main/java/com/sangwon/oauth/conf/SecurityConfig.java
+++ src/main/java/com/sangwon/oauth/conf/SecurityConfig.java
@@ -3,14 +3,18 @@ package com.sangwon.oauth.conf;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
@@ -34,11 +38,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers("/","/login/**","/auth/**").permitAll()
 		.antMatchers("/kakao").hasAuthority("ROLE_KAKAO")
 		.antMatchers("/naver").hasAuthority("ROLE_NAVER")
+		.antMatchers("/google").hasAuthority("ROLE_GOOGLE")
 		.anyRequest().authenticated()
 			.and()
 		.oauth2Login()
 		.userInfoEndpoint()
-		.userService(new CustomOAuth2UserService())
+		.userService(new CustomOAuth2UserService()) //네이버 USER INFO의 응답을 처리하기 위한 설정
 			.and() 
 		.defaultSuccessUrl("/login-success")
 		.failureUrl("/login-failure")
@@ -49,12 +54,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Bean
 	public ClientRegistrationRepository clientRegistrationRepository
-	(@Value("${custom.oauth2.kakao.client-id}") String kakaoClientId,
+	(OAuth2ClientProperties oAuth2ClientProperties,
+	 @Value("${custom.oauth2.kakao.client-id}") String kakaoClientId,
 	 @Value("${custom.oauth2.kakao.client-secret}") String kakaoClientSecret,
 	 @Value("${custom.oauth2.naver.client-id}") String naverClientId,
 	 @Value("${custom.oauth2.naver.client-id}") String naverClientSecret){
 
-		List<ClientRegistration> registrations = new ArrayList<>();
+		List<ClientRegistration> registrations = oAuth2ClientProperties
+				.getRegistration().keySet().stream()
+				.map(client -> getRegistration(oAuth2ClientProperties, client))
+				.filter(Objects::nonNull)
+				.collect(Collectors.toList());
+
 		registrations.add(CustomOAuth2Provider.KAKAO.getBuilder("kakao")
 				.clientId(kakaoClientId)
 				.clientSecret(kakaoClientSecret)
@@ -70,7 +81,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		return new InMemoryClientRegistrationRepository(registrations);
 	}
 
+	private ClientRegistration getRegistration(OAuth2ClientProperties clientProperties, String client) {
 
-
+		if("google".equals(client)) {
+			OAuth2ClientProperties.Registration registration = clientProperties.getRegistration().get("google");
+			return CommonOAuth2Provider.GOOGLE.getBuilder(client)
+					.clientId(registration.getClientId())
+					.clientSecret(registration.getClientSecret())
+					.scope("email", "profile") .build();
+		}
+		return null;
+	}
 
 }
